8aa7b659edeec2c13d53482fc22a3c4bfef53e9f
Huỳnh Chí Bảo <devblue404@gmail.com>
2025-12-11 00:00:24 +0700

    Refactor VNPAY configuration for local and hosting

--- bookstore/config/vnpay_config.php
+++ bookstore/config/vnpay_config.php
@@ -1,20 +1,13 @@
 <?php
-// Tự động nhận môi trường local hoặc hosting
-if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
 
-    // Đường dẫn LOCAL 
-    $vnp_return = 'http://localhost/thuc-hanh-php/bookstore/unibook_vnpay_callback.php';
-
-} else {
-
-    // Đường dẫn HOSTING
-    $vnp_return = 'https://huynhchibao.42web.io/bookstore/unibook_vnpay_callback.php';
-}
+$is_local = ($_SERVER['HTTP_HOST'] === 'localhost');
 
 return [
-    'vnp_TmnCode'     => 'Q21B8B0O',
-    'vnp_HashSecret'  => '5ECX8H6CE0I35756JVLOJXJ48LI8UJTY',
-    'vnp_Url'         => 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
-    'vnp_ReturnUrl'   => $vnp_return,
-    'vnp_Api'         => 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction'
+    'vnp_TmnCode'    => 'Q21B8B0O',
+    'vnp_HashSecret' => '5ECX8H6CE0I35756JVLOJXJ48LI8UJTY',
+    'vnp_Url'        => 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+    'vnp_ReturnUrl'  => $is_local 
+        ? 'http://localhost/thuc-hanh-php/bookstore/vnpay_callback.php'
+        : 'https://huynhchibao.42web.io/bookstore/vnpay_callback.php',
 ];
+?>
